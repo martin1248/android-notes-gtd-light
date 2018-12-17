c61295a38f805bfca55598a1bdfb83307161aa9a
Martin1248 <martinonlinedrive@gmail.com>
2018-12-17 16:14:26 +0100

    Added DatePickerDialog for due date

--- app/src/main/java/io/github/martin1248/gtdlight/EditorActivity.java
+++ app/src/main/java/io/github/martin1248/gtdlight/EditorActivity.java
@@ -1,5 +1,6 @@
 package io.github.martin1248.gtdlight;
 
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,15 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements
+        View.OnClickListener{
 
     private String action;
     private EditText editorText;
@@ -31,6 +37,9 @@ public class EditorActivity extends AppCompatActivity {
     private String oldContext;
     private String oldProject;
     private String oldDueDate;
+
+    Button btnDatePicker;
+    private int mYear, mMonth, mDay;
 
     private String[] states = {"Inbox", "Next actions", "Calender", "Some day/maybe", "Waiting for", "Reference", "Trash"};
     private String[] contexts = {".Home", ".Office", ".Shopping list"};
@@ -61,6 +70,8 @@ public class EditorActivity extends AppCompatActivity {
         editorProject.setThreshold(1);
         editorProject.setAdapter(adapterProjects);
 
+        btnDatePicker=(Button)findViewById(R.id.selectDateButton);
+        btnDatePicker.setOnClickListener(this);
 
         Intent intent = getIntent();
 
@@ -134,6 +145,32 @@ public class EditorActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finishEditing();
+    }
+    //endregion
+
+    //region View.OnClickListener
+    @Override
+    public void onClick(View v) {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        editorDueDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        datePickerDialog.show();
     }
     //endregion
 
